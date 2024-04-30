97e85a2b130afe703e43dac30df19a49b084fa3a
Junobee25 <wnsdhqo@naver.com>
2024-04-30 14:42:03 +0900

    initial commit

---
+++ src/main/java/com/sideproject/shoescream/ShoescreamApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.shoescream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoescreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoescreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/shoescream/ShoescreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.shoescream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoescreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
