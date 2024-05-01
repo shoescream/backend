31ad8a27e5e99d6beec4bbfb410ab371968dfb4c
Junobee25 <wnsdhqo@naver.com>
2024-05-01 13:59:09 +0900

    initial commit

--- src/main/java/com/sideproject/shoescream/ShoesCreamApplication.java
+++ src/main/java/com/sideproject/shoescream/ShoesCreamApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoescreamApplication {
+public class ShoesCreamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoescreamApplication.class, args);
+		SpringApplication.run(ShoesCreamApplication.class, args);
 	}
 
 }
