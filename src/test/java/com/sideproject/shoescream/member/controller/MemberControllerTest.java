9ee582706003e8e90fee871034de5c4a592fa396
Junobee25 <wnsdhqo@naver.com>
2024-05-06 20:01:57 +0900

    test: 로그인 컨트롤러 단위 테스트 구현

--- src/test/java/com/sideproject/shoescream/member/controller/MemberControllerTest.java
+++ src/test/java/com/sideproject/shoescream/member/controller/MemberControllerTest.java
@@ -2,8 +2,11 @@ package com.sideproject.shoescream.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sideproject.shoescream.global.dto.response.Response;
+import com.sideproject.shoescream.member.dto.request.MemberSignInRequest;
 import com.sideproject.shoescream.member.dto.request.MemberSignUpRequest;
 import com.sideproject.shoescream.member.dto.response.MemberResponse;
+import com.sideproject.shoescream.member.dto.response.MemberSignInResponse;
+import com.sideproject.shoescream.member.dto.response.TokenResponse;
 import com.sideproject.shoescream.member.service.EmailService;
 import com.sideproject.shoescream.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
@@ -40,20 +43,10 @@ class MemberControllerTest {
     @Test
     @DisplayName("[POST] 회원 가입 컨트롤러 테스트")
     @WithMockUser
-    void signUp() throws Exception {
+    void 유저_회원가입_컨트롤러_테스트() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        MemberSignUpRequest request = MemberSignUpRequest.builder()
-                .memberId("qownsdh")
-                .email("wnsdhqo@naver.com")
-                .password("1234")
-                .name("배준오")
-                .build();
-
-        MemberResponse response = MemberResponse.builder()
-                .memberId("qownsdh")
-                .email("wnsdhqo@naver.com")
-                .name("배준오")
-                .build();
+        MemberSignUpRequest request = createMemberSignUpRequest();
+        MemberResponse response = createMemberResponse();
 
         String requestContent = mapper.writeValueAsString(request);
         String responseContent = mapper.writeValueAsString(Response.success(response));
@@ -66,5 +59,64 @@ class MemberControllerTest {
                         .content(requestContent))
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseContent));
+    }
+
+    @Test
+    @DisplayName("[POST] 로그인 컨트롤러 테스트")
+    @WithMockUser
+    void 유저_로그인_컨트롤러_테스트() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        MemberSignInRequest request = createMemberSignInRequest();
+        MemberSignInResponse response = createMemberSignInResponse();
+
+        String requestContent = mapper.writeValueAsString(request);
+        String responseContent = mapper.writeValueAsString(Response.success(response));
+
+        given(memberService.signIn(any(MemberSignInRequest.class))).willReturn(response);
+
+        mockMvc.perform(post("/signin").with(csrf())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseContent));
+    }
+
+    private MemberSignInRequest createMemberSignInRequest() {
+        return MemberSignInRequest.builder()
+                .memberId("qownsdh")
+                .password("1234")
+                .build();
+    }
+
+    private MemberSignInResponse createMemberSignInResponse() {
+        return MemberSignInResponse.builder()
+                .memberResponse(createMemberResponse())
+                .tokenResponse(createTokenResponse())
+                .build();
+    }
+
+    private MemberSignUpRequest createMemberSignUpRequest() {
+        return MemberSignUpRequest.builder()
+                .memberId("qownsdh")
+                .email("wnsdhqo@naver.com")
+                .password("1234")
+                .name("배준오")
+                .build();
+    }
+
+    private MemberResponse createMemberResponse() {
+        return MemberResponse.builder()
+                .memberId("qownsdh")
+                .email("wnsdhqo@naver.com")
+                .name("배준오")
+                .build();
+    }
+
+    private TokenResponse createTokenResponse() {
+        return TokenResponse.builder()
+                .accessToken("123123123123123123")
+                .refreshToken("123123123123123123")
+                .build();
     }
 }
